2c70570c9940abeb6c67b63d69a57fc68d5faa65
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-12-16 09:09:07 +0200

    Tomcat fixed

--- src/main/java/mate/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package mate.service;
-
-import javax.naming.AuthenticationException;
-import mate.model.Driver;
-
-public interface AuthenticationService {
-    Driver login(String login, String password) throws AuthenticationException;
-}
